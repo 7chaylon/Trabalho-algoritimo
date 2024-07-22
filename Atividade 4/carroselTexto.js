00078de5bfae67b299f426c4c73a5e5bf6857366
Ariel <146725507+Baterista15@users.noreply.github.com>
2024-07-22 20:00:36 -0300

    Add files via upload

--- Atividade 4/carroselTexto.js
+++ Atividade 4/carroselTexto.js
@@ -1,53 +1,72 @@
 const itemsCarrossel = [
   {
-    imagem:
-      'https://https://sudotec.org.br/upload/aprendercrescerdepoimentos/dest_7.jpg.placeholder.com/150',
-    depoimento:
-      'O projeto Aprender e Crescer foi um dos marcos do meu início de carreira na área de tecnologia e programação, caí de para-quedas, com o envio da inscrição de um amigo. Trabalhava no RH de uma empresa na qual pedi transferência para o setor do T.',
-    descricao: '- GABRIELA FARIAS - AC - 2016',
+      imagem: 'https://sudotec.org.br/upload/aprendercrescerdepoimentos/dest_6.jpg',
+      depoimento: '"O curso aprender & crescer foi uma das coisas mais importantes que aconteceu comigo em 2019, se não a mais importante, pois..."',
+      descricao: '-GABRIEL NOVOCHADLEY - AC-2019'
   },
   {
-    imagem:
-      'https://sudotec.org.br/upload/aprendercrescerdepoimentos/dest_2.jpg',
-    depoimento:
-      'O projeto Aprender de carreira na área de tecnologia e programação, caí de para-quedas, com o envio da inscrição de um amigo. Trabalhava no RH de uma empresa na qual pedi transferência para o setor do T.',
-    descricao: '- GABRIEL - AC - 2019',
+      imagem: 'https://sudotec.org.br/upload/aprendercrescerdepoimentos/dest_8.jpg',
+      depoimento: 'O Projeto Aprender & Crescer foi o que viabilizou a mudança em minha vida, vinha trabalhando a 5 anos no mercado de trabalho em um ramo onde não me identificava, e meu sonho sempre foi atuar na área de tecnologia, foi quando descobr...',
+      descricao: '- Carlos Eduardo Bageston - AC 2022'
   },
   {
-    imagem:
-      'https://sudotec.org.br/upload/aprendercrescerdepoimentos/dest_8.jpg',
-    depoimento:
-      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
-    descricao: '- Jorge - AC - 2022',
+      imagem: 'https://sudotec.org.br/upload/aprendercrescerdepoimentos/dest_2.jpg',
+      depoimento: 'O projeto Aprender e Crescer foi o ponto de ' + "'start'" + ' na minha vida na área de tecnologia, após passar pelo proces...',
+      descricao: '- Jorge - AC - 2022'
   },
-]
+  {
+      imagem: 'https://sudotec.org.br/upload/aprendercrescerdepoimentos/dest_4.jpg',
+      depoimento: '“Vim de outra cidade para cursar Engenharia de Software na UTFPR e em paralelo iniciei o curso Aprender e Crescer da Sudotec, como uma...”',
+      descricao: '-GABRIELA  FARIAS - AC - 2016'
+  },
+  {
+      imagem: 'https://sudotec.org.br/upload/aprendercrescerdepoimentos/dest_1.jpg',
+      depoimento: '“O projeto  Aprender e Crescer me deu muitas oportunidades, desde 2016 quando participei pela primeira vez do projeto me encantei com o curso, motivo pelo qual me levou a faze-lo novamente em 2017, um curso excelente , com ótimos prof...',
+      descricao: '-CRISTIANE BORDIGNON MONTEIRO - AC 2016 e 2017'
+  },
+  {
+      imagem: 'https://sudotec.org.br/upload/aprendercrescerdepoimentos/dest_5.jpg',
+      depoimento: '“O curso Aprender e Crescer foi uma experiência incrível, pois além de todo o aprendizado adquirido, na área...',
+      descricao: '-PEDRO LUCAS MEURER - AC - 2019'
+  },
+  {
+      imagem: 'https://sudotec.org.br/upload/aprendercrescerdepoimentos/dest_7.jpg',
+      depoimento: '“O projeto Aprender e Crescer foi um dos marcos do meu início de carreira na área de tecnologia e programação, caí de para-quedas, com o envio da inscrição de um amigo...',
+      descricao: '- Bernardo Bortot Spiazzi - AC 2021'
+  },
+];
 
-let indiceAtual = 0
+let indiceAtual = 0;
 
 function trocaOItemAtualDoCarrossel() {
-  const imagemRef = document.getElementById('imagem-carrossel')
-  const depoimentoRef = document.getElementById('carrossel-depoimento')
-  const descricaof = document.getElementById(`imagem-carrosel`)
+  const imagemRef = document.getElementById('imagem-carrossel');
+  const depoimentoRef = document.getElementById('carrossel-depoimento');
+  const descricaoRef = document.getElementById('carrossel-descricao');
 
-  const { imagem, depoimento, descricao } = itemsCarrossel[indiceAtual]
+  const { imagem, depoimento, descricao } = itemsCarrossel[indiceAtual];
 
-  imagemRef.src = imagem
-  depoimentoRef.innerText = depoimento
-  descricaof.innerText = descricao
+  imagemRef.src = imagem;
+  depoimentoRef.innerText = depoimento;
+  descricaoRef.innerText = descricao;
 }
 
 function avancarCarrossel() {
-  indiceAtual++
+  indiceAtual++;
 
-  if (indiceAtual === itemsCarrossel.length) indiceAtual = 0
+  if (indiceAtual === itemsCarrossel.length) indiceAtual = 0;
 
-  trocaOItemAtualDoCarrossel()
+  trocaOItemAtualDoCarrossel();
 }
 
 function voltarCarrossel() {
-  indiceAtual--
+  indiceAtual--;
 
-  if (indiceAtual < 0) indiceAtual = itemsCarrossel.length - 1
+  if (indiceAtual < 0) indiceAtual = itemsCarrossel.length - 1;
 
-  trocaOItemAtualDoCarrossel()
+  trocaOItemAtualDoCarrossel();
 }
+setInterval(() => {
+avancarCarrossel();
+}, 3000)
+
+document.addEventListener('DOMContentLoaded', trocaOItemAtualDoCarrossel);
